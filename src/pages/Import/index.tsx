a9ca235a02b8e3618da6cb653edc35aa478749b7
nkkf <nelsonkenmochi@gmail.com>
2020-07-14 22:37:28 -0400

    first commit

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
+
+    for (let i = 0; i < uploadedFiles.length; i++) {
+      const file = uploadedFiles[i].file;
+      data.append('file', file);
+    }
 
     // TODO
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
     // TODO
+    setUploadedFiles(files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: file.size.toString(),
+    } as FileProps)));
   }
 
   return (
